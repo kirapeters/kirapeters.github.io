6bec0239dfb78e78bcb9ea6d6121726441f2ffd1
Kira <56455045+kiracherie@users.noreply.github.com>
2023-09-06 15:20:25 -0600

    a nice responsive header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,16 @@
 import './Header.css';
 
+// todo: add an active class to the tab that is active
 function Header() {
   return (
     <header>
       <div className="home-button">
         <button>Kira Peters</button>
       </div>
-      <div className="nav-links">
+      <nav className="nav-links">
         <a href="www.linkedin.com/in/kira-peters01">LinkedIn</a>
         <a href="https://github.com/kirapeters">Github</a>
-      </div>
+      </nav>
     </header>
   );
 }
